88b9a811ba5db3a4f1d6d7a3bc742e7a89349d7e
Ingrid Santos <ingridmsantosp13@gmail.com>
2017-11-27 22:00:23 -0200

    ..

--- kmeans.py
+++ kmeans.py
@@ -3,35 +3,45 @@ from math import sqrt
 
 dic = {}
 d = {}
+nlists = {}
 keys = []
+
 def main():
 	file = sys.argv[1]
 	k = int(sys.argv[2])
 	iterations = int(sys.argv[3])
-	centroids = {1:(11.111,8.644),2:(13.222,5.444),3:(8.988,11.444)} # so pra teste aleatorio
+	centroids = {0:(11.111,8.644),1:(13.222,5.444),2:(8.988,11.444)} # so pra teste aleatorio
 	# for i in range(0,iterations):
-	#    centroids[i] = (randomx,randomy)     
-    
+	#    centroids[i] = (randomx,randomy)
+	nlists = {0:(0,0),1:(0,0),2:(0,0)}
+	
 	F = open(file,"r")
+	c = 0
 	for line in F:
-		words = line.split()
-		point = words[1],words[2]
-		dic[words[0]] = point
-		keys.append(words[0])
+	    if c != 0 :
+		    words = line.split()
+		    point = words[1],words[2]
+		    dic[words[0]] = point
+		    keys.append(words[0])
+	    c = c+1
 	F.close()
 	kmeans(centroids,k,iterations)
 	
 def kmeans(cent,k,it):
+
     for x in range(0,it):
 	    for i in range(len(keys)):
-	        menor = sqrt(((dic[keys[i]][0]-cent[0][0])^2) + ((dic[keys[i]][1]-cent[0][1])^2))
+	        menor = sqrt((pow(float(dic[keys[i]][0])-cent[0][0],2)) + (pow(float(dic[keys[i]][1])-cent[0][1],2)))
 	        num = 0
 	        for z in range(1,it):
-	            dist = sqrt(((dic[keys[i]][0]-cent[z][0])^2) + ((dic[keys[i]][1]-cent[z][1])^2))
+	            dist = sqrt((pow(float(dic[keys[i]][0])-cent[z][0],2)) + (pow(float(dic[keys[i]][1])-cent[z][1],2)))
 	            if dist < menor:
+	                menor = dist
 	                num = z
+	                
 	        d[keys[i]] = num
-    print(d)	    	        			
+
+    print(d)            
 
 def calcCentroid(clist):
 	cont = 0
