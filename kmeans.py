99c750ee52732237623688b974452ccad7aa78a0
Ingrid Santos <ingridmsantosp13@gmail.com>
2017-11-30 11:39:08 -0200

    add comentarios

--- kmeans.py
+++ kmeans.py
@@ -2,12 +2,12 @@ import sys
 from math import sqrt
 import numpy as np
 
-dic = {}
-d = {}
-sumx = []
-sumy = []
-nums = []
-keys = []
+dic = {} # dicionario que possui ids e coordenadas
+d = {} # estrutura resultante da iteracao
+sumx = [] # soma valores em x (pra cada cluster)
+sumy = [] # soma valores em y (pra cada cluster)
+nums = [] # quantidade de elementos em cada cluster
+keys = [] # vetor de ids 
 
 def main():
 	file = sys.argv[1]
@@ -24,38 +24,38 @@ def main():
 	F = open(file,"r")
 	c = 0
 	for line in F:
-	    if c != 0 :
-		    words = line.split()
-		    point = words[1],words[2]
+	    if c != 0 : # ignora a primeira linha do arquivo
+		    words = line.split() # pega cada linha e faz o split nos espacos em branco
+		    point = words[1],words[2] # coordenadas
 		    dic[words[0]] = point
 		    keys.append(words[0])
 	    c = c+1
 	F.close()
 	res = kmeans(centroids,k,iterations,sumx,sumy,nums)
-	#print(res)
 	
 	
 def kmeans(cent,k,it,sumx,sumy,nums):
     c = cent
     for x in range(0,it):
         print(c)
-        d = aux(c,k,it,sumx,sumy,nums)
+        d = aux(c,k,it,sumx,sumy,nums) # faz uma iteracao
         for a in range(0,k):
-            c[a] = str(sumx[a]/nums[a])+","+str(sumy[a]/nums[a])
+            c[a] = str(sumx[a]/nums[a])+","+str(sumy[a]/nums[a]) # recalcula os centroids
             
     return d	
+
 	
 def aux(cent,k,it,sumx,sumy,nums):
 
-    for b in range(0,k):
+    for b in range(0,k): # zera as somas e numero de elementos de cada cluster
         sumx[b] = 0
         sumy[b] = 0
         nums[b] = 0
     
-    for i in range(len(keys)):
-        c = cent[0].split(',')
-        menor = sqrt((pow(float(dic[keys[i]][0])-float(c[0]),2)) + (pow(float(dic[keys[i]][1])-float(c[1]),2)))
-        num = 0
+    for i in range(len(keys)): 
+        c = cent[0].split(',') # faz o split pra pegar os valores do centroid 
+        menor = sqrt((pow(float(dic[keys[i]][0])-float(c[0]),2)) + (pow(float(dic[keys[i]][1])-float(c[1]),2))) # seta o menor valor
+        num = 0 # seta o valor de qual centroid o ponto pertence ate o momento
         for z in range(1,k):
             c = cent[z].split(',')
             dist = sqrt((pow(float(dic[keys[i]][0])-float(c[0]),2)) + (pow(float(dic[keys[i]][1])-float(c[1]),2)))
@@ -63,8 +63,9 @@ def aux(cent,k,it,sumx,sumy,nums):
                 menor = dist
                 num = z
                 
-        d[keys[i]] = num
+        d[keys[i]] = num 
         print(d)
+	# atualiza as somas e numero de elementos do cluster
         sumx[num] += float(dic[keys[i]][0])
         sumy[num] += float(dic[keys[i]][1])
         nums[num] += 1
