fd5ea77944dcd7c71f617af3d33a1cfb3ed9ad83
Giovana Morais <giovana.vmorais@gmail.com>
2017-12-13 08:30:28 -0200

    merge, min, dist

--- single_link.py
+++ single_link.py
@@ -1,6 +1,7 @@
 import sys
 import numpy as np
 import scipy.sparse as ss
+from collections import deque
 
 def main():
     particoes, identificador = le_arquivo(sys.argv[1])
@@ -8,30 +9,52 @@ def main():
     kMax = int(sys.argv[3])
 
     tam_matriz = qtd_particoes(sys.argv[1])
+    iteracoes = tam_matriz - kMax
     matriz = cria_matriz_inicial(particoes, tam_matriz)
-    #print(matriz)
-    minimo, teste_x, teste_y = acha_minimo(matriz, tam_matriz)
-    print("A menor distância é entre os clusters {} e {}".format(identificador[teste_x], identificador[teste_y]))
-    print("teste linha matriz pro cluster {}: {}".format( identificador[teste_x], matriz[teste_x][:]))
-    print("teste linha matriz pro cluster {}: {}".format( identificador[teste_y], matriz[teste_y][:]))
 
+    print("-----------MATRIZ INICIAL---------\n{}\n".format(matriz))
+    for i in range(0, 5):
+        print("\nITERAÇÃO {}".format(i))
+        minimo, x, y = acha_minimo(matriz, tam_matriz)
+        # descarta o primeiro mínimo
+        matriz[x][y] = 0
+        matriz = merge(x, y, matriz, tam_matriz)
+        # exclui primeira coluna
+        matriz = np.delete(matriz, 0, 1)
+        tam_matriz -= 1
+        print("A menor distância é entre os clusters {} e {}, com mínimo igual a {}".format(identificador[x], identificador[y], minimo))
+        if(x > y):
+            # adiciona x ao cluster de y
+            identificador[y] = str(identificador[y]) + ", " + str(identificador[x])
+            del identificador[x]
+            print(identificador)
+        else:
+            # adiciona y ao cluster de x
+            identificador[x] = str(identificador[x]) + ", " + str(identificador[y])
+            del identificador[y]
+            print(identificador)
+        print(matriz)
 
 def le_arquivo(dataset):
     nlinhas = qtd_particoes(dataset)
     particoes = np.zeros((nlinhas, 2))
     identificador = []
 
-    with open(dataset) as f:
-        next(f)
-        i = 0
-        for linha in f:
-            parametros = linha.replace('\n', '').split('\t')
-            identificador.append(parametros[0])
-            if(i == nlinhas - 1):
-                break
-            particoes[i,0] = float(parametros[1])
-            particoes[i,1] = float(parametros[2])
-            i += 1
+    try:
+        with open(dataset) as f:
+            next(f)
+            i = 0
+            for linha in f:
+                parametros = linha.replace('\n', '').split('\t')
+                identificador.append(parametros[0])
+                if(i == nlinhas - 1):
+                    break
+                particoes[i,0] = float(parametros[1])
+                particoes[i,1] = float(parametros[2])
+                i += 1
+    except EnvironmentError as erro:
+        print("Verifique se os parâmetros do seu arquivo estão separados por tabs.")
+        print(erro)
     f.close()
     return particoes, identificador
 
@@ -53,8 +76,6 @@ def cria_matriz_inicial(particoes, tam_matriz):
         inteiro com a quantidade de particoes
     Retorno
     -------
-    matriz_esparsa  :   lil_matrix
-        matriz esparsa com todas as distâncias calculadas
     minimo  : int
         primeira distância mínima
     """
@@ -62,8 +83,6 @@ def cria_matriz_inicial(particoes, tam_matriz):
     for linha in range(0, tam_matriz):
         for coluna in range(linha+1, tam_matriz):
             matriz[linha][coluna] = dist(particoes[linha][0], particoes[coluna][0], particoes[linha][1], particoes[coluna][1])
-   # matriz_esparsa = ss.lil_matrix(matriz)
-   # return matriz_esparsa
     return matriz
 
 def dist(x0, x1, y0, y1):
@@ -72,30 +91,45 @@ def dist(x0, x1, y0, y1):
     """
     return np.sqrt((x1 - x0)**2 + (y1 - y0)**2)
 
-
 def acha_minimo(matriz, tam_matriz):
     """
     Acha o mínimo da matriz desconsiderando diagonais
     """
-    minimo = matriz[0, 1]
     pos_x = 0
     pos_y = 1
+    minimo = matriz[0][0]
+    for i in range(0, tam_matriz):
+        if(matriz[0][i] != 0):
+            minimo = matriz[0][i]
+
     for linha in range(0, tam_matriz):
+        # dá pra manter esse for pq agora a matriz continua triangular
         for coluna in range(linha+1, tam_matriz):
-            if(matriz[linha, coluna] < minimo):
-                minimo = matriz[linha, coluna]
+        #for coluna in range(0, tam_matriz):
+            if(matriz[linha, coluna] < minimo and matriz[linha, coluna]> 0):
+                minimo = matriz[linha][coluna]
                 pos_x = linha
                 pos_y = coluna
     return minimo, pos_x, pos_y;
 
-def merge(cA, cB, matriz):
+def merge(cA, cB, matriz, tam_matriz):
     """
     Faz o merge entre os clusters de menor distância euclidiana
     """
 
-    #comparar matriz[cA][:] c/ matriz[cB][:] e manter quem for menor
-
-    return "nada acontece feijoada"
+    for i in range(0, tam_matriz):
+        #print("cA: {}".format(matriz[cA][i]))
+        #print("cB: {}".format(matriz[cB][i]))
+        # checar coluna
+        if(matriz[cA][i] > 0 and matriz[cB][i] > 0 and matriz[cA][i] <= matriz[cB][i]):
+            matriz[cB][i] = 0
+        elif(matriz[cA][i] > matriz[cB][i] and matriz[cB][i] > 0):
+            matriz[cA][i] = matriz[cB][i]
+            matriz[cB][i] = 0
+        else:
+            pass
+    matriz = np.delete(matriz, cB, axis=0)
+    return matriz
 
 if __name__ == '__main__':
     main()
